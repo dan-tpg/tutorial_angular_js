b90f553fa237dd5f89027a6f5da74e9083a915ec
dan-tpg <Danilo@tpg-wks03.TPGDesign.local>
2015-10-12 13:05:31 +0100

    index - showing list of all the available examples.

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
  <!DOCTYPE html>
 <html lang="en-US">
 <script src="/assets/js/angular.min.js"></script>
+<script src="/assets/js/jquery-2.1.4.min.js"></script>
 <header><title>Tutorial - Angular JS</title></header>
 <body>
 <h1>Tutorial - Angular JS</h1>
+<script>
+$.ajax({
+	  url: "http://angularjs/pages/",
+	  success: function(data){
+	     $(data).find("a:contains(.php)").each(function(){
+	        // will loop through 
+	        var file = $(this).attr("href");
+
+	        $('<a href="/pages/'+file+'"></a><br>').html(file).appendTo('#listLinks')
+	     });
+	  }
+	});
+</script>
+<div id="listLinks"></div>
 </body>
 </html> 
